aec9aeaedc309bd12492f0f1ab1531379a84c0ed
abzeelaw <118025806+abzeelaw@users.noreply.github.com>
2024-09-19 09:36:48 +0100

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,12 @@
+let num0 =0
+
+document.getElementById("display-board").textContent = num0
+
+let display = document.getElementById("display-board")
+
+let count = 0
+
+function one(){
+    
+    display.innerText=count
+}
